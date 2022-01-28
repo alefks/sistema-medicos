1e4135895d907f37de384c969a897da0be6072fc
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2022-01-28 17:29:39 -0300

    feat(app.module.ts): adiciona aos imports o PrismaModule, MedicoModule e EspecialidadeModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+import { PrismaModule } from './prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MedicoModule } from './medico/medico.module';
+import { EspecialidadeModule } from './especialidade/especialidade.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, MedicoModule, EspecialidadeModule],
   controllers: [AppController],
   providers: [AppService],
 })
