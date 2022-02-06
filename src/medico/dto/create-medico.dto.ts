7d266b5919262088609a4e21b0d210c288d33e18
Alessandro Kobashigawa <ale.kobashigawa@outlook.com>
2022-02-06 04:37:19 -0300

    refactor(create-medico.dto): adiciona o MaxLength e o MinLength

--- src/medico/dto/create-medico.dto.ts
+++ src/medico/dto/create-medico.dto.ts
@@ -4,22 +4,29 @@ import {
   IsNotEmpty,
   IsNumberString,
   IsOptional,
+  MaxLength,
+  MinLength,
 } from 'class-validator';
 
 export class CreateMedicoDto {
   @IsNotEmpty({ message: 'Insira um nome.' })
+  @MaxLength(120)
   nome: string;
 
   @IsNotEmpty({ message: 'Insira o seu CRM.' })
   @IsNumberString()
+  @MaxLength(7)
+  @MinLength(7)
   crm: string;
 
   @IsNotEmpty({ message: 'Insira um telefone fixo.' })
   @IsNumberString()
+  @MaxLength(12)
   telefone_fixo: string;
 
   @IsNotEmpty({ message: 'Insira um telefone celular.' })
   @IsNumberString()
+  @MaxLength(12)
   telefone_celular: string;
 
   @IsInt({ each: true })
